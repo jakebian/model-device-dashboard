d45bde543346558e1abf8a824fd262468583abe1
Jake Bian <jake@thousandeyes.com>
2015-04-08 23:32:53 -0700

    dashboard styling and dynamic colors

--- assets/js/src/constants.js
+++ assets/js/src/constants.js
@@ -3,23 +3,28 @@ angular.module('tte.constants', [])
     {
         'new': {
             id: 'new',
-            name: 'Newly Created'
+            name: 'Newly Created',
+            color: '#3F51B5'
         },
         prod: {
             id: 'prod',
-            name: 'In Production'
+            name: 'In Production',
+            color: '#1565C0'
         },
         transit: {
             id: 'transit',
-            name: 'In Transit'
+            name: 'In Transit',
+            color: '#558B2F'
         },
         deployed: {
             id: 'deployed',
-            name: 'Deployed'
+            name: 'Deployed',
+            color: '#EF6C00'
         },
         expired: {
             id: 'expired',
-            name: 'Expired'
+            name: 'Expired',
+            color: '#E64A19'
         },
     }
 );
